5ebacda3b81d862a58770744bd684fe22b3e2931
Kwangwook-Kwon <kwangwook.kwon@gmail.com>
2018-11-29 07:21:56 +0900

    test

--- force-directed-graph.js
+++ force-directed-graph.js
@@ -7,7 +7,7 @@ d3.csv("tweet_list", function (data) {
 
 let tweet_data = [];
 let key = [];
-d3.json("./Twitter_Data/RetweetNew/163819").then(draw_force_directed_graph)
+d3.json("./Twitter_Data/RetweetNew/163819",
 
 function draw_force_directed_graph(data) {
 
@@ -17,93 +17,85 @@ function draw_force_directed_graph(data) {
 
   let nodes = d3.range(key.length).map(function (i) {
     return {
-      index: i
+      id: i
     };
   });
 
-  let links = d3.range(key.length - 1).map(function (i) {
+  let links = d3.range(key.length).map(function (i) {
     return {
       source: i,
-      target: find_parent_tweet_index(i)
+      target: find_parent_tweet_index(i),
+      value : i
     };
   });
 
-  function find_parent_tweet_index(i){
+  function find_parent_tweet_index(i) {
     //console.log(data[key[i]].parent_tweet)
     let parent = data[key[i]].parent_tweet;
-    for(let k = 0 ; k< key.length; k++){
-      if(parent == data[key[k]].tweet)
-        return k; 
+    for (let k = 0; k < key.length; k++) {
+      if (parent == data[key[k]].tweet)
+        return k;
     }
   }
-
-  let simulation = d3.forceSimulation(nodes)
+  console.log(links);
+  var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+  var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) { return d.id; }))
     .force("charge", d3.forceManyBody())
-    .force("link", d3.forceLink(links).distance(20).strength(1))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY())
-    .on("tick", ticked);
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-  let canvas = document.querySelector("canvas"),
-    context = canvas.getContext("2d"),
-    width = canvas.width,
-    height = canvas.height;
+  var link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+    .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
 
-  d3.select(canvas)
+  var node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+    .attr("r", 2)
+    .attr("fill", function (d) { return color(d.group); })
     .call(d3.drag()
-      .container(canvas)
-      .subject(dragsubject)
       .on("start", dragstarted)
       .on("drag", dragged)
       .on("end", dragended));
 
+  node.append("title")
+    .text(function (d) { return d.id; });
+  simulation
+    .nodes(nodes)
+    .on("tick", ticked);
+  simulation.force("link")
+    .links(links);
   function ticked() {
-    context.clearRect(0, 0, width, height);
-    context.save();
-    context.translate(width / 2, height / 2);
-
-    context.beginPath();
-    links.forEach(drawLink);
-    context.strokeStyle = "#aaa";
-    context.stroke();
-
-    context.beginPath();
-    nodes.forEach(drawNode);
-    context.fill();
-    context.strokeStyle = "#fff";
-    context.stroke();
-
-    context.restore();
+    link
+      .attr("x1", function (d) { return d.source.x; })
+      .attr("y1", function (d) { return d.source.y; })
+      .attr("x2", function (d) { return d.target.x; })
+      .attr("y2", function (d) { return d.target.y; });
+    node
+      .attr("cx", function (d) { return d.x; })
+      .attr("cy", function (d) { return d.y; });
   }
 
-  function dragsubject() {
-    return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2);
-  }
-
-  function dragstarted() {
+  function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d3.event.subject.fx = d3.event.subject.x;
-    d3.event.subject.fy = d3.event.subject.y;
+    d.fx = d.x;
+    d.fy = d.y;
   }
-
-  function dragged() {
-    d3.event.subject.fx = d3.event.x;
-    d3.event.subject.fy = d3.event.y;
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
   }
-
-  function dragended() {
+  function dragended(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
-    d3.event.subject.fx = null;
-    d3.event.subject.fy = null;
+    d.fx = null;
+    d.fy = null;
   }
-
-  function drawLink(d) {
-    context.moveTo(d.source.x, d.source.y);
-    context.lineTo(d.target.x, d.target.y);
-  }
-
-  function drawNode(d) {
-    context.moveTo(d.x + 3, d.y);
-    context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
-  }
-}
+})
