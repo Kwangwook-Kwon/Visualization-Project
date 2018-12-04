bc484bd0cf62a478984086ae6839fc7651330176
Kwangwook-Kwon <kwangwook.kwon@gmail.com>
2018-12-04 16:58:06 +0900

    small bug

--- menu_area.js
+++ menu_area.js
@@ -6,16 +6,16 @@ function combobox(data) {
         .attr('class', 'select')
         .on('change', onchange)
 
-    var options = select
+    select
         .selectAll('option')
         .data(data).enter()
         .append('option')
         .text(function (d) { return d.number; })
-        .attr('selected',function(d){
-            if(d.number == 28000)
+        .attr('selected', function (d) {
+            if (d.number == 28000)
                 return 'selected';
         })
-        initial_draw_tree(28000)
+    initial_draw_tree(28000)
 
     function onchange() {
         selectValue = d3.select('select').property('value')
