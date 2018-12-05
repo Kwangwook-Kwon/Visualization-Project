41279ec43588d9fec504ce9390f59bdf408d593c
Kwangwook-Kwon <kwangwook.kwon@gmail.com>
2018-12-05 11:09:46 +0900

    Refactoring

--- force-directed-graph.js
+++ force-directed-graph.js
@@ -6,6 +6,14 @@ let key = [];
 let max_depth = 0;
 
 let svg = d3.select('body').append('svg').attr('width', tree_svg_width).attr('height', tree_svg_height).attr("id", "Tree")
+svg.append("rect").attr("id", "border")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("height", tree_svg_height)
+  .attr("width", tree_svg_width)
+  .style("stroke", 'black')
+  .style("fill", "white")
+  .style("stroke-width", 1);
 let simulation = d3.forceSimulation()
 let node, nodes, link, links
 let zoom_handler
@@ -14,11 +22,11 @@ let loaded = 0
 let file_list = [];
 
 function initial_draw_tree(selected_file) {
-  d3.json("./Twitter_Data/RetweetNew/"+selected_file+".json").then(draw_force_directed_graph)
+  d3.json("./Twitter_Data/RetweetNew/" + selected_file + ".json").then(draw_force_directed_graph)
 }
 
-function select_update_tree(selected_file){
-  d3.json("./Twitter_Data/RetweetNew/"+selected_file+".json").then(update_tree)
+function select_update_tree(selected_file) {
+  d3.json("./Twitter_Data/RetweetNew/" + selected_file + ".json").then(update_tree)
 }
 
 function draw_force_directed_graph(data) {
@@ -29,34 +37,9 @@ function draw_force_directed_graph(data) {
 
 function initialize_graph() {
   loaded = 0
+  key = Object.keys(input_data)
 
-  let svg = d3.select('#Tree')
-  let key = Object.keys(input_data)
-
-  nodes = d3.range(key.length).map(function (i) {
-    if (max_depth < input_data[key[i]].depth)
-      max_depth = +input_data[key[i]].depth;
-    return {
-      id: key[i],
-      depth: input_data[key[i]].depth,
-      child: input_data[key[i]].child,
-      parent: input_data[key[i]].parent_tweet,
-      bot: input_data[key[i]].bot
-    };
-  });
-
-  links = d3.range(key.length).map(function (i) {
-    return {
-      key: i,
-      source: key[i],
-      target: input_data[key[i]].parent_tweet,
-      depth: input_data[key[i]].depth,
-      child: input_data[key[i]].child,
-      bot: input_data[key[i]].bot
-    };
-  }).filter(function (d) {
-    return d.source != d.target;
-  });
+  prepare_data()
 
   simulation
     .nodes(nodes)
@@ -72,94 +55,14 @@ function initialize_graph() {
     .force("link", d3.forceLink(links).distance(10).strength(1).id(function (d) { return d.id; }))
     .on("tick", ticked)
 
-  link = svg.append("g")
-    .attr("class", "links").attr('id', "links")
-    .selectAll("line")
-    .data(links)
-    .enter()
-    .append("line")
-    .attr("id", d => 'ID' + d.source.id + 'to' + d.target.id)
-    .attr("stroke-tree_svg_width", 1)
-    .attr("stroke", '#999')
-    .attr("opacity", 0.6)
-
-  node = svg.append("g")
-    .attr("class", "nodes").attr("id", "nodes")
-    .selectAll("circle")
-    .data(nodes)
-    .enter()
-    .append("circle")
-    .attr("id", d => 'ID' + d.id)
-    .attr("r", function (d) {
-      if (d.depth == 1)
-        return max_circle_radius;
-      else if (d.child == 0 && d.depth == 2)
-        return 1;
-      else
-        return max_circle_radius;
-    })
-    .attr("fill", function (d) {
-      if (d.bot == 1)
-        return 'red';
-      else
-        return 'blue';
-    })
-    .call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended))
-
-  svg.append("rect").attr("id", "border")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("height", tree_svg_height)
-    .attr("width", tree_svg_width)
-    .style("stroke", 'black')
-    .style("fill", "white")
-    .style("stroke-width", 1);
-
-  svg.append("g").attr("id", "loading").append("text")
-    .attr("dy", "0.35em")
-    .attr("text-anchor", "middle")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 40)
-    .attr('x', tree_svg_width / 2)
-    .attr('y', tree_svg_height / 2)
-    .text("Simulating...");
-
-  d3.select("#loading").append("rect").attr("id", "bar")
-    .attr('x', tree_svg_width / 2 - 200)
-    .attr('y', (tree_svg_height / 2) + 30)
-    .attr("height", 40)
-    .attr("tree_svg_width", 0)
-
-  d3.select("#loading").append("rect")
-    .attr('x', tree_svg_width / 2 - 200)
-    .attr('y', (tree_svg_height / 2) + 30)
-    .attr("height", 40)
-    .attr("width", 400)
-    .style("fill", "none")
-    .style("stroke-tree_svg_width", 1)
-    .style("stroke", 'black')
-
-  d3.select("#loading").append("text").attr("id", "progress")
-    .attr("dy", "0.35em")
-    .attr("text-anchor", "middle")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 40)
-    .attr('x', tree_svg_width / 2)
-    .attr('y', (tree_svg_height / 2) + 50)
-    .text("0 %");
-
-  events();
+  draw_nodes_links()
 }
 
-function update_tree(data){
+function update_tree(data) {
   input_data = data
   loaded = 0
 
-  let svg = d3.select('#Tree')
-  let key = Object.keys(input_data)
+  key = Object.keys(input_data)
   nodes = []
   links = []
   node = []
@@ -168,6 +71,32 @@ function update_tree(data){
   d3.select("#links").remove()
   d3.select("#loading").remove()
 
+  prepare_data()
+
+  simulation
+    .nodes(nodes)
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .force("charge", d3.forceManyBody().strength(function (d) {
+      if (d.child == 0 && d.depth <= 2)
+        return -1;
+      else
+        return -15;
+    }))
+    .force("center", d3.forceCenter(tree_svg_width / 2, tree_svg_height / 2))
+    .force("link", d3.forceLink(links).distance(10).strength(1).id(function (d) { return d.id; }))
+    .on("tick", ticked)
+    .alpha(1).restart()
+
+
+  svg.select("#border")
+    .transition()
+    .style("fill", "none")
+    
+  draw_nodes_links()
+}
+
+function prepare_data() {
 
   nodes = d3.range(key.length).map(function (i) {
     if (max_depth < input_data[key[i]].depth)
@@ -194,22 +123,9 @@ function update_tree(data){
     return d.source != d.target;
   });
 
+}
 
-  simulation
-  .nodes(nodes)
-  .force("x", d3.forceX())
-  .force("y", d3.forceY())
-  .force("charge", d3.forceManyBody().strength(function (d) {
-    if (d.child == 0 && d.depth <= 2)
-      return -1;
-    else
-      return -15;
-  }))
-  .force("center", d3.forceCenter(tree_svg_width / 2, tree_svg_height / 2))
-  .force("link", d3.forceLink(links).distance(10).strength(1).id(function (d) { return d.id; }))
-  .on("tick", ticked)
-  .alpha(1).restart()
-
+function draw_nodes_links() {
 
   link = svg.append("g")
     .attr("class", "links").attr('id', "links")
@@ -247,10 +163,6 @@ function update_tree(data){
       .on("start", dragstarted)
       .on("drag", dragged)
       .on("end", dragended))
-
-  svg.select("#border")
-    .transition()
-    .style("fill", "none")
 
   svg.append("g").attr("id", "loading").append("text")
     .attr("dy", "0.35em")
@@ -286,7 +198,6 @@ function update_tree(data){
     .text("0 %");
 
   events();
-
 }
 
 function zoom_actions() {
